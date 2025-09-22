b92ca167e82d38c3725a6fce25d49f3a4198ddaf
Lucas Guidine <lucas.guidine@dimensa.com.br>
2025-09-22 12:39:08 -0300

    fix:improving dto readability

--- src/pack/dtos/pack-response.dto.ts
+++ src/pack/dtos/pack-response.dto.ts
@@ -3,13 +3,24 @@ import { Type } from 'class-transformer';
 import { IsNumber, IsArray, ValidateNested, IsOptional } from 'class-validator';
 
 export class PackedBoxDto {
-  @ApiProperty({ nullable: true }) @IsOptional() caixa_id: string | null;
-  @ApiProperty({ type: [String] }) @IsArray() produtos: string[];
-  @ApiProperty({ required: false }) @IsOptional() observacao?: string;
+  @ApiProperty({ nullable: true })
+  @IsOptional()
+  caixa_id: string | null;
+
+  @ApiProperty({ type: [String] })
+  @IsArray()
+  produtos: string[];
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  observacao?: string;
 }
 
 export class RequestResponseDto {
-  @ApiProperty() @IsNumber() pedido_id: number;
+  @ApiProperty()
+  @IsNumber()
+  pedido_id: number;
+
   @ApiProperty({ type: [PackedBoxDto] })
   @IsArray()
   @ValidateNested({ each: true })
