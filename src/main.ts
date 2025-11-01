0bbae7a959c93526ed854eebed405c5db928b0b4
Lucas Guidine <lucas.guidine@dimensa.com.br>
2025-11-01 13:07:11 -0300

    feat:adding global prefix

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('packing-service');
 
   app.useGlobalPipes(
     new ValidationPipe({
